4c3e8eab200ad0da896fcbe73c646d84535a208c
wsy <3298210337@qq.com>
2023-06-16 20:15:31 +0800

    last time commit, version:1.0.

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.http import Http404
 from .models import Topic, Entry
@@ -20,7 +20,8 @@ def topics(request):
 @login_required
 def topic(request, topic_id):
 	"""显示单个主题及其所有的条目"""
-	topic = Topic.objects.get(id=topic_id)
+	#topic = Topic.objects.get(id=topic_id)
+	topic = get_object_or_404(Topic, id=topic_id)#优化一下错误页面
 
 	check_topic_owner(topic, request)
 
@@ -54,7 +55,8 @@ def new_topic(request):
 @login_required
 def new_entry(request, topic_id):
 	"""在特定主题中添加新条目"""
-	topic = Topic.objects.get(id=topic_id)
+	#topic = Topic.objects.get(id=topic_id)
+	topic = get_object_or_404(Topic, id=topic_id)
 
 	check_topic_owner(topic, request)
 
@@ -77,7 +79,8 @@ def new_entry(request, topic_id):
 @login_required
 def edit_entry(request, entry_id):
 	"""编辑既有的条目"""
-	entry = Entry.objects.get(id=entry_id)
+	#entry = Entry.objects.get(id=entry_id)
+	entry = get_object_or_404(Entry, id=entry_id)
 	topic = entry.topic
 
 	check_topic_owner(topic, request)
